bcf5195803d1c382ac13a91d15423e2782fc2015
unknown <alarafath71@gmail.com>
2022-02-28 23:54:43 +0600

    card box added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log('ok')
+const loadPhones = () => {
+    const url = 'https://openapi.programming-hero.com/api/phones?search=iphone'
+    fetch(url)
+    .then(res => res.json())
+    .then(allData => displayPhones(allData.data))
+}
+loadPhones();
+const displayPhones = phones =>{
+    phones.forEach(phone =>{
+        console.log(phone)
+    })
+}
