e62e590fea47b558b08eed68a3780ca2338515ce
unknown <alarafath71@gmail.com>
2022-03-01 08:51:37 +0600

    All phones data added

--- js/app.js
+++ js/app.js
@@ -6,7 +6,25 @@ const loadPhones = () => {
 }
 loadPhones();
 const displayPhones = phones =>{
+    // console.log(phones)
+    const phonesDiv = document.getElementById('phones');
     phones.forEach(phone =>{
         console.log(phone)
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="col">
+        <div class="card h-100 shadow-sm rounded">
+          <div class="text-center mt-3">
+            <img  src="${phone.image}" class="card-img-top" alt="...">
+          </div>
+          <div class="card-body">
+            <h5 class="card-title">${phone.phone_name}</h5>
+            <p class="card-text">${phone.brand}</p>
+            <button type="button" class="btn btn-primary">More Details</button>
+          </div>
+        </div>
+      </div>
+        `
+        phonesDiv.appendChild(div)
     })
 }
